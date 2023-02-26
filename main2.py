30f19f37d3a2ff31372fe984326232b6876a9178
Ganesh-o9 <125977896+Ganesh-o9@users.noreply.github.com>
2023-02-26 22:22:47 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -352,7 +352,7 @@ async def account_login(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}+bestaudio" --hls-prefer-ffmpeg --no-keep-video --remux-video mkv "{url}" -o "{name}.%(ext)s"'
 
             try:
-                Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-**\n`{url}`\n\n"
+                Show = f"**Downloading:-**\n\n**Name :-**\n`{name}\nQuality - {raw_text2}`\n\n**Url :-**\n`{url}`\n\n"
                 prog = await m.reply_text(Show)
                 cc = f'{str(count).zfill(3)}.  {name1} {res}.mkv\n\n**Batch:-** {raw_text0}\n\n'
                 cc1 = f'{str(count).zfill(3)}. {name1} {res}.pdf\n\n**Batch:-** {raw_text0}\n\n'
